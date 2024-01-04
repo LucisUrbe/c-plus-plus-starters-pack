5c5261b2b4c48623ae8bd1a8801548eb526c11a8
GilbertJin <17175403+GilbertJin@users.noreply.github.com>
2024-01-04 14:59:04 +0800

    New files: 06 & 07

---
+++ basic/06-sort-and-search.cc
@@ -0,0 +1,107 @@
+///
+/// @file       06-sort-and-search.cc
+/// @author     Gilbert Jin
+/// @copyright  Shanghai Jiao Tong University, 2019
+/// @date       January 3, 2024
+/// @version    0.2
+/// @brief      The use of bubble sort and binary search.
+///
+
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+    // Step 1: use a 1-d array to store a 4x4 matrix and input all the integers
+    int a[16], low, high, mid, input;
+    bool finished, right = true, down = true;
+    std::cout << "Input: ";
+    for (int i = 0; i < 16; i++)
+        std::cin >> a[i];
+    // Step 2: use bubble sort, time complexity: O(n*n)
+    for (int i = 1; i < 16; i++) {
+        // Here the boolean is for determining an ordered array
+        finished = true;
+        for (int j = 0; j < 16 - i; j++) {
+            if (a[j] < a[j + 1]) {
+                finished = false;
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+        if (finished)
+            break;
+    }
+    // Step 3: use binary search to find the input integer
+    std::cout << "Input: ";
+    std::cin >> input;
+    std::cout << "Output: ";
+    low = 0;
+    high = 15;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (input == a[mid])
+            break;
+        if (input < a[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    if (low > high)
+        std::cout << "Not Found" << std::endl;
+    else {
+        // The question is deteministic so just enumerate!
+        switch (mid) {
+            case 0:
+                std::cout << "0, 0" << std::endl;
+                break;
+            case 1:
+                std::cout << "0, 1" << std::endl;
+                break;
+            case 2:
+                std::cout << "0, 2" << std::endl;
+                break;
+            case 3:
+                std::cout << "0, 3" << std::endl;
+                break;
+            case 4:
+                std::cout << "1, 3" << std::endl;
+                break;
+            case 5:
+                std::cout << "2, 3" << std::endl;
+                break;
+            case 6:
+                std::cout << "3, 3" << std::endl;
+                break;
+            case 7:
+                std::cout << "3, 2" << std::endl;
+                break;
+            case 8:
+                std::cout << "3, 1" << std::endl;
+                break;
+            case 9:
+                std::cout << "3, 0" << std::endl;
+                break;
+            case 10:
+                std::cout << "2, 0" << std::endl;
+                break;
+            case 11:
+                std::cout << "1, 0" << std::endl;
+                break;
+            case 12:
+                std::cout << "1, 1" << std::endl;
+                break;
+            case 13:
+                std::cout << "1, 2" << std::endl;
+                break;
+            case 14:
+                std::cout << "2, 2" << std::endl;
+                break;
+            case 15:
+                std::cout << "2, 1" << std::endl;
+                break;
+            default:
+                std::cout << "Internal Method Error. Sorry about this!" << std::endl;
+        }
+    }
+    return 0;
+}
---
+++ basic/07-rearrange-words.cc
@@ -0,0 +1,68 @@
+///
+/// @file       07-rearrange-words.cc
+/// @author     Gilbert Jin
+/// @copyright  Shanghai Jiao Tong University, 2019
+/// @date       January 3, 2024
+/// @version    0.2
+/// @brief      The use of math theorem, permutation and logic.
+///
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    char words[10][11], sentence[110], key = ' ';
+    int counter = 0, length[10] = {0}, total_length[10] = {0};
+    // Step 1: input a sentence without a full stop(.)
+    std::cout << "Input: ";
+    std::cin.getline(sentence, 110);
+    std::cout << "Output: ";
+    // Step 2: count how many words the sentence has
+    for (int i = 0; sentence[i] != '\0'; i++) {
+        if (key == ' ' && sentence[i] != ' ')
+            counter++;
+        key = sentence[i];
+    }
+    // Step 3: count how many characters each word has
+    key = ' ';
+    for (int i = -1, j = 0; i < counter && key != '\0'; j++) {
+        if (key == ' ' && sentence[j] != ' ')
+            i++;
+        while (sentence[j] != '\0')
+            if (sentence[j] != ' ') {
+                length[i]++;
+                j++;
+            }
+            else
+                break;
+        key = sentence[j];
+    }
+    // Step 4: split the words and store them into an array
+    for (int i = 0, k = 0; i < counter; i++) {
+        for (int j = 0; j < length[i]; j++, k++)
+            words[i][j] = sentence[k];
+        while (sentence[k] == ' ')
+            k++;
+    }
+    // Step 5: calculate total lengths of the words with
+    //          the same possible length (1~10)
+    for (int i = 1; i <= 10; i++)
+        for (int j = 0; j < counter; j++)
+            if (length[j] == i)
+                total_length[i - 1] += i;
+    // Step 6: throughtout traverse, output the words with 
+    //          the same length pieced-together with respect to
+    //          the total length, from shorter to longer
+    // The total lengths are small so just brute force
+    for (int sum = 1; sum <= 100; sum++) 
+        for (int i = 0; i < 10; i++) 
+            if (total_length[i] == sum) {
+                for (int j = 0; j < counter; j++)
+                    if (length[j] == i + 1)
+                        for (int k = 0; k < length[j]; k++)
+                            std::cout << words[j][k];
+                
+                std::cout << ' ';
+            }
+    std::cout << std::flush;
+    return 0;
+}

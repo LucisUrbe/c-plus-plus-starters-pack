59ca084f131f8c1cd4300c1792178a6176e2b2eb
LucisUrbe <pm.lu@pm.me>
2024-04-28 01:13:22 +0800

    New: more sort methods

--- structures/set/sort.hh
+++ structures/set/sort.hh
@@ -103,3 +103,136 @@ void bubbleSort(SET<KEY, VALUE>*& toSort, int size) {
             }
     }
 }
+
+// the up-down-divide function of the quick sort according to the pivot(low)
+template <typename KEY, typename VALUE>
+int divide(SET<KEY, VALUE>*& toDivide, int low, int high) {
+    SET<KEY, VALUE> standard = toDivide[low];
+    do {
+        while (low < high && toDivide[high].key >= standard.key)
+            high--; // from high to low, find the first element smaller than the pivot
+        if (low < high) {
+            // covering the low with the high, the first one covered is "standard",
+            // the second one being the empty place from the last large loop
+            toDivide[low] = toDivide[high];
+            low++;
+        }
+        while (low < high && toDivide[low].key <= standard.key)
+            low++; // check from low to high until the first element bigger than the pivot is met
+        if (low < high) {
+            // covering the high with the low, the first one covered is the empty place from the last small loop
+            toDivide[high] = toDivide[low];
+            high--;
+        }
+    } while (low != high);
+    toDivide[low] = standard; // low == high, which is the place that the pivot should be
+    return low;
+}
+// the actual quick sort function
+template <typename KEY, typename VALUE>
+void quickSort(SET<KEY, VALUE>*& toSort, int low, int high) {
+    if (low >= high)
+        return;
+    int mid = divide(toSort, low, high);
+    quickSort(toSort, low, mid - 1); // the "mid" element is at the right place; now sort the remaining ones
+    quickSort(toSort, mid + 1, high);
+}
+// Quick sort: O(n*lb(n))~(n^2)
+template <typename KEY, typename VALUE>
+void quickSort(SET<KEY, VALUE>*& toSort, int size) {
+    quickSort(toSort, 0, size - 1);
+}
+
+
+// the merge function of merge sort; Note that what to be merged are the two organized halves of a list
+template <typename KEY, typename VALUE>
+void merge(SET<KEY, VALUE>*& toMerge, int left, int mid, int right) {
+    SET<KEY, VALUE>* result = new SET<KEY, VALUE>[right - left + 1];
+    // i for the left half (left ~ mid-1), j for the right half (mid ~ right), k for the result (left ~ right)
+    int i = left, j = mid, k = 0;
+    while (i < mid && j <= right)
+        if (toMerge[i].key < toMerge[j].key)
+            result[k++] = toMerge[i++]; // The smaller one goes into the result
+        else
+            result[k++] = toMerge[j++];
+    while (i < mid) // Directly merge the remaining of the left half
+        result[k++] = toMerge[i++];
+    while (j <= right) // The right half
+        result[k++] = toMerge[j++];
+    for (int i = 0, j = left; j <= right; i++, j++)
+        toMerge[j] = result[i]; // give back the result
+    delete [] result;
+}
+// the actual function of merge sort
+template <typename KEY, typename VALUE>
+void mergeSort(SET<KEY, VALUE>*& toSort, int left, int right) {
+    if (left == right)
+        return;
+    int mid = (left + right) / 2;
+    mergeSort(toSort, left, mid);
+    mergeSort(toSort, mid + 1, right);
+    merge(toSort, left, mid + 1, right);
+}
+// Merge sort: O(n*lb(n))
+template <typename KEY, typename VALUE>
+void mergeSort(SET<KEY, VALUE>*& toSort, int size) {
+    mergeSort(toSort, 0, size - 1);
+}
+
+// Integer bucket sort: O(n*carry)
+// Assuming that the keys are decimal integers
+template <typename KEY, typename VALUE>
+void bucketSort(SET<KEY, VALUE>*& toSort, int size) {
+    struct setNode {
+        SET<KEY, VALUE> data;
+        setNode* next;
+        setNode(): next(nullptr) {}
+        setNode(SET<KEY, VALUE>& rawData, setNode* rawNode): data(rawData), next(rawNode) {} // KEY must be int
+        ~setNode() {}
+    };
+    // new feature: convert toSort to actualList
+    setNode* actualList = nullptr;
+    for (int i = 0; i < size; i++)
+        actualList = new setNode(toSort[size - 1 - i], actualList); // this behaves like a stack
+    // the following algorithm will find the maximum number of digits for a key
+    int max = 0;
+    for (setNode* tail = actualList; tail != nullptr; tail = tail -> next)
+        if (tail -> data.key > max)
+            max = tail -> data.key;
+    int carry = 0;
+    if (max != 0)
+        while (max > 0) {
+            carry++;
+            max /= 10;
+        }
+    // the actual algorithm is as follows
+    for (int i = 1, base = 1; i < carry; i++, base *= 10) {
+        setNode* first[10], *last[10];
+        for (int j = 0; j < 10; j++)
+            first[j] = last[j] = nullptr; // initialize the bucket
+        while (actualList != nullptr) {
+            int position = actualList -> data.key / base % 10; // find which bucket the node goes to
+            if (first[position] == nullptr)
+                first[position] = last[position] = actualList;
+            else
+                last[position] = last[position] -> next = actualList;
+            actualList = actualList -> next;
+        }
+        setNode* tail = nullptr;
+        for (int j = 0; j < 10; j++) {
+            if (first[j] == nullptr)
+                continue;
+            if (tail == nullptr)
+                actualList = tail = first[j];
+            else
+                tail -> next = first[j];
+            tail = last[j];
+        }
+        tail -> next = nullptr;
+    }
+    // convert actualList to toSort
+    for (int i = 0; i < size; i++) {
+        toSort[i] = actualList -> data;
+        actualList = actualList -> next;
+    }
+}
